c723bccc5589946a0eb18e0e11891e00feef07a2
Limah-T <limahtechnology@yahoo.com>
2025-05-28 15:02:03 +0100

    Restructure the home url to '' to trigger homepage when web app url is clicked.

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     path('account/', include('account.urls')),
     path('chat/', include('chat.urls')),
     path('api/', include('api.urls')),
