e635a148880ba6f42c9a43a4c87e441170a49b41
Limah-T <limahtechnology@yahoo.com>
2025-06-06 16:13:22 +0100

    Create a super user account..

--- home/views.py
+++ home/views.py
@@ -2,8 +2,4 @@ from django.views.generic.base import TemplateView
 from account.models import CustomUser
 
 class HomeView(TemplateView):
-    user = CustomUser.objects.get(username="limah")
-    user.is_staff = True
-    user.is_superuser = True
-    user.save()
     template_name = "home/home.html"
