85c677333ae0c709a2257285140c8041e8b907c4
Limah-T <limahtechnology@yahoo.com>
2025-06-07 08:06:09 +0100

    Debugging production errore..

--- api/views.py
+++ api/views.py
@@ -106,6 +106,7 @@ class LoginView(views.APIView):
         print(serializer.validated_data)
         email = serializer.validated_data.get("email")
         password = serializer.validated_data.get("password")
+        print(CustomUser.objects.values())
         user = authenticate(request, email=email, password=password)
         print(user)
         if not user:   
